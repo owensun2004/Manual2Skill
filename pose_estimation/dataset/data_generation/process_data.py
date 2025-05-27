3974bfab7925c5990066965b4d6863baeedb7ed2
Liu Yiwei <133667590+LEw1sin@users.noreply.github.com>
2025-05-27 20:06:49 +0800

    Update process_data.py

--- pose_estimation/dataset/data_generation/process_data.py
+++ pose_estimation/dataset/data_generation/process_data.py
@@ -69,13 +69,13 @@ def normalize_and_randomize_point_cloud(point_cloud, num_data):
     center = np.mean(point_cloud, axis=1).reshape(b*M, 1, 3)
     point_cloud_normalized = (point_cloud - center)# / np.std(point_cloud, axis=0)
 
-    # 随机生成一个四元数
-    random_quaternion = np.random.randn(b*M,4)  # 生成一个长度为4的随机向量
-    random_quaternion /= np.linalg.norm(random_quaternion, axis=0)  # 归一化为单位四元数
-    # 将四元数转换为旋转矩阵
+    # Generate a quaternion randomly
+    random_quaternion = np.random.randn(b*M,4)  
+    random_quaternion /= np.linalg.norm(random_quaternion, axis=0)  
+    # Convert the quaternion into a rotation matrix
     rotation_matrix = quan2rot(random_quaternion)
 
-    # 随机旋转点云
+    # Randomly rotate the point cloud
     point_cloud_normalized = np.matmul(point_cloud_normalized ,rotation_matrix.transpose(0, 2, 1))
 
     point_cloud_normalized = point_cloud_normalized.reshape(b, M, N, 3)
@@ -84,32 +84,32 @@ def normalize_and_randomize_point_cloud(point_cloud, num_data):
     
     return point_cloud_normalized, rotation_matrix, center
 
-# 使用PCA对批量点云进行规范化
+# Use PCA to canonicalize the batch point clouds
 def canonicalize_point_cloud_batch(point_cloud_batch):
-    B = point_cloud_batch.shape[0]  # 获取batch大小
+    B = point_cloud_batch.shape[0]  # Obtain the batch size
     canonicalized_pcd_batch = []
     rotation_matrices = []
     centroids = []
     for b in range(B):
-        # 1. 获取单个点云
+        # 1. Obtain a single point cloud
         point_cloud = point_cloud_batch[b]
-        # 2. 中心化点云（使其均值为0）
+        # 2. Centralized point cloud (making its mean value 0)
         centroid = np.mean(point_cloud, axis=0)
         centered_pcd = point_cloud - centroid
-        # 3. 使用PCA
+        # 3. use PCA
         pca = PCA(n_components=3)
         pca.fit(centered_pcd)
-        # 4. 获取旋转矩阵（主轴方向）
+        # 4. Obtain the rotation matrix (principal axis direction)
         rotation_matrix = pca.components_
         if np.linalg.det(rotation_matrix) < 0:
             rotation_matrix = -rotation_matrix
-        # 5. 将点云投影到主轴上
+        # 5. Project the point cloud onto the principal axis
         transformed_pcd = pca.transform(centered_pcd)
-        # 6. 存储结果
+        # 6. Store the result
         canonicalized_pcd_batch.append(transformed_pcd)
         rotation_matrices.append(rotation_matrix)
         centroids.append(centroid)
-    # 将结果转为 NumPy 数组
+    # Convert the result to a NumPy array
     canonicalized_pcd_batch = np.array(canonicalized_pcd_batch)
     rotation_matrices = np.array(rotation_matrices)
     centroids = np.array(centroids)
@@ -117,48 +117,48 @@ def canonicalize_point_cloud_batch(point_cloud_batch):
     return canonicalized_pcd_batch, rotation_matrices, centroids
 
 def canonicalize_point_cloud_batch_heu(point_cloud_batch, k=3):
-    B = point_cloud_batch.shape[0]  # 获取batch大小
+    B = point_cloud_batch.shape[0]  # Obtain the batch size
     canonicalized_pcd_batch = []
     rotation_matrices = []
     centroids = []
     for b in range(B):
-        # 1. 获取单个点云
+        # 1. Obtain a single point cloud
         point_cloud = point_cloud_batch[b]
-        # 2. 中心化点云（使其均值为0）
+        # 2. Centralized point cloud (making its mean value 0)
         center = np.mean(point_cloud, axis=0)
 
         point_cloud = point_cloud - center
-        # 计算每个点到质心的距离
+        # Calculate the distance from each point to the center of mass
         distances = np.linalg.norm(point_cloud, axis=1)
-        # 找到离质心最远的 k 个点的索引
+        # Find the index of the k points farthest from the center of mass
         farthest_k_indices = np.argsort(distances)[-k:]
-        # 计算最远的 k 个点和质心连线的方向
+        # Calculate the direction of the line connecting the farthest k points and the center of mass
         directions = point_cloud[farthest_k_indices]
-        # 计算平均方向作为 x 轴
+        # Calculate the average direction as the X-axis
         x_axis = np.mean(directions, axis=0)
-        x_axis /= np.linalg.norm(x_axis)  # 归一化 x 轴向量
+        x_axis /= np.linalg.norm(x_axis)  # Normalize the X-axis vector
         
-        # 将所有点投影到与 x 轴正交的平面
+        # Project all the points onto the plane orthogonal to the X-axis
         projected_cloud = point_cloud - np.outer(np.dot(point_cloud, x_axis), x_axis)
-        # 找到投影后的点中离质心最远的 k 个点的索引
+        # Find the index of the k points farthest from the center of mass among the projected points
         projected_distances = np.linalg.norm(projected_cloud, axis=1)
         projected_farthest_k_indices = np.argsort(projected_distances)[-k:]
-        # 计算投影后的 k 个点和质心连线的方向作为 y 轴
+        # Calculate the direction of the line connecting the k projected points and the center of mass as the Y-axis
         projected_directions = projected_cloud[projected_farthest_k_indices]
         y_axis = np.mean(projected_directions, axis=0)
-        y_axis /= np.linalg.norm(y_axis)  # 归一化 y 轴向量
+        y_axis /= np.linalg.norm(y_axis)  # Normalize the Y-axis vector
         
-        # 根据右手系规则计算 z 轴，即 z_axis = x_axis × y_axis
+        # Calculate the Z-axis according to the right-hand system rule, that is, z_axis = x_axis × y_axis
         z_axis = np.cross(x_axis, y_axis)
         
-        # 构建旋转矩阵
+        # Construct the rotation matrix
         rotation_matrix = np.array([x_axis, y_axis, z_axis]).T
         # print(rotation_matrix)
         canonical_cloud = point_cloud  @ rotation_matrix
         canonicalized_pcd_batch.append(canonical_cloud)
         rotation_matrices.append(rotation_matrix.T)
         centroids.append(center)
-    # 将结果转为 NumPy 数组
+    # Convert the result to a NumPy array
     canonicalized_pcd_batch = np.array(canonicalized_pcd_batch)
     rotation_matrices = np.array(rotation_matrices)
     centroids = np.array(centroids)
@@ -202,13 +202,7 @@ def main(args):
         reconed_point_cloud = randomized_point_cloud @ rotation_matrix + center[:, :, None, :]
         if args.visualize:
         
-            # 创建点云对象
-            # pts1 = trimesh.points.PointCloud(randomized_point_cloud[0][0])
-            # pts2 = trimesh.points.PointCloud(randomized_point_cloud[0][1])
-
-            # pts1 = trimesh.points.PointCloud(point_clouds[0])
-            # pts2 = trimesh.points.PointCloud(point_clouds[1])
-            # 创建一个空的场景
+            # Create an empty scene
             scene = trimesh.Scene()
             num_parts = reconed_point_cloud.shape[1]
             for i in range(num_parts):
@@ -216,43 +210,31 @@ def main(args):
                 pcd.visual.vertex_colors = np.random.randint(0, 255, size=(3))
                 scene.add_geometry(pcd)
 
-            # # 在场景中指定位置创建一个红色小球，作为红点
-            # # 使用一个小球代表红点，设置球体的位置
-            # red_dot_position = camera_pose[:3, 3]  # 小球的位置为相机的位置
-            # red_dot = trimesh.creation.icosphere(subdivisions=2, radius=0.05)  # 创建一个半径为0.05的小球
-            # red_dot.apply_translation(red_dot_position)  # 移动小球到指定位置
 
-            # # 将红点添加到场景中
-            # red_dot.visual.vertex_colors = [255, 0, 0, 255]  # 设置小球的颜色为红色
-            # scene.add_geometry(red_dot)
-            # 创建坐标系（原点处的三条轴）
+            x_axis = trimesh.creation.cylinder(radius=0.02, height=1)  # Create a cylinder with a radius of 0.02
+            # x_axis.apply_translation([0.5, 0, 0])  # Translate along the X-axis direction
 
-            # 创建X轴（红色），它指向x方向
-            x_axis = trimesh.creation.cylinder(radius=0.02, height=1)  # 创建一个半径为0.02的圆柱体
-            # x_axis.apply_translation([0.5, 0, 0])  # 沿x轴方向平移
-
-            # 创建Y轴（绿色），它指向y方向，绕X轴旋转90°
+            # Create the Y-axis (green), which points in the Y direction and rotates 90° around the X-axis
             y_axis = trimesh.creation.cylinder(radius=0.02, height=1)
-            # y_axis.apply_translation([0, 0.5, 0])  # 沿y轴方向平移
-            rotation_matrix_y = transformations.rotation_matrix(np.pi / 2, [1, 0, 0])  # 绕x轴旋转90°
-            y_axis.apply_transform(rotation_matrix_y)  # 应用旋转矩阵
+            # y_axis.apply_translation([0, 0.5, 0])  # Translate along the Y-axis direction
+            rotation_matrix_y = transformations.rotation_matrix(np.pi / 2, [1, 0, 0])  # Rotate 90° around the X-axis
+            y_axis.apply_transform(rotation_matrix_y)  
 
-            # 创建Z轴（蓝色），它指向z方向，绕Y轴旋转90°
+            # Create the Z-axis (blue), which points in the Z direction and rotates 90° around the Y-axis
             z_axis = trimesh.creation.cylinder(radius=0.02, height=1)
-            # z_axis.apply_translation([0, 0, 0.5])  # 沿z轴方向平移
-            rotation_matrix_z = transformations.rotation_matrix(-np.pi / 2, [0, 1, 0])  # 绕y轴旋转90°（负方向）
-            z_axis.apply_transform(rotation_matrix_z)  # 应用旋转矩阵
+            # z_axis.apply_translation([0, 0, 0.5])  # Translate along the Z-axis direction
+            rotation_matrix_z = transformations.rotation_matrix(-np.pi / 2, [0, 1, 0])  # Rotate 90° around the Y-axis (in the negative direction)
+            z_axis.apply_transform(rotation_matrix_z)  
 
-            # 设置坐标轴的颜色
-            x_axis.visual.vertex_colors = [255, 0, 0, 255]  # X轴为红色
-            y_axis.visual.vertex_colors = [0, 255, 0, 255]  # Y轴为绿色
-            z_axis.visual.vertex_colors = [0, 0, 255, 255]  # Z轴为蓝色
+            x_axis.visual.vertex_colors = [255, 0, 0, 255]  
+            y_axis.visual.vertex_colors = [0, 255, 0, 255]  
+            z_axis.visual.vertex_colors = [0, 0, 255, 255]  
 
-            # 将坐标轴添加到场景中
+            # Add the coordinate axes to the scene
             scene.add_geometry(x_axis)
             scene.add_geometry(y_axis)
             scene.add_geometry(z_axis)
-            # 可视化
+
             scene.show()
 
 if __name__ == "__main__":
